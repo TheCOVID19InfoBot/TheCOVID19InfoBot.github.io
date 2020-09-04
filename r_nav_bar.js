023bb10b3baf70a0522759a7f4230ea089dbae85
Donald Lee <51865552+Donald-K-Lee@users.noreply.github.com>
2020-09-04 01:19:15 -0700

    Built a responsive navigation bar

--- r_nav_bar.js
+++ r_nav_bar.js
@@ -1,32 +1,43 @@
-//Responsive Nav Bar
-var TurnsNBOn = document.getElementById("TheResponsiveNavBar");
-var addani = document.getElementById("addsnavani");
-var hambar = document.getElementById("randomfactstext");
-var hambar2 = document.getElementById("randomfactstexton");
-var hambaroff = document.getElementsByClassName("rnavbutton")[0];
-var hambaron = document.getElementsByClassName("rnavbuttonon")[0];
-var bg = document.getElementsByClassName("TheResponsiveNavBarbackground")[0];
-// When the user clicks the button, open the bar 
-hambar.onclick = function() {
-	TurnsNBOn.style.display = "block";
-  	addani.classList.add("rnavanimation");
-	hambaroff.style.display = "none";
-	hambaron.style.display = "inline";
-	bg.style.display = "inline";
+//Actual mobile responsive nav bar
+var mobile_r_nav = document.getElementById("responsive_mobile_navbar");
+var outsideofnavbg = document.getElementsByClassName("TheResponsiveNavBarbackground")[0];
+
+//Plus button on click
+function navbarfunction() {
+	var mobile_r_nav = document.getElementById("responsive_mobile_navbar");
+	var outsideofnavbg = document.getElementsByClassName("TheResponsiveNavBarbackground")[0];
+	outsideofnavbg.style.display = "inline";
+	mobile_r_nav.classList.remove("mobile_responsive_navbar_close");
+	mobile_r_nav.classList.add("mobile_responsive_navbar_open");
+	var theplusbtn = document.getElementById("plusbtn");
+	var theplusbtnon = document.getElementById("plusbtnon");
+	outsideofnavbg.style.display = "inline";
+	var onplus = document.getElementsByClassName("on")[0];
+	onplus.style.display = "inline";
+	var offplus = document.getElementsByClassName("off")[0];
+	offplus.style.display = "none";
 }
-// When the user clicks on the button again, close the modal
-hambar2.onclick = function() {
-    TurnsNBOn.style.display = "block";
-	addani.classList.remove("rnavanimation");
-	hambaroff.style.display = "inline";
-	hambaron.style.display = "none";
-	bg.style.display = "none";
+//Plus button on click again
+function navbarfunctionclose() {
+	var mobile_r_nav = document.getElementById("responsive_mobile_navbar");
+	mobile_r_nav.classList.remove("mobile_responsive_navbar_open");
+	mobile_r_nav.classList.add("mobile_responsive_navbar_close");
+	var outsideofnavbg = document.getElementsByClassName("TheResponsiveNavBarbackground")[0];
+	outsideofnavbg.style.display = "none";
+	var onplus = document.getElementsByClassName("on")[0];
+	onplus.style.display = "none";
+	var offplus = document.getElementsByClassName("off")[0];
+	offplus.style.display = "inline";
 }
 // When the user clicks anywhere outside of the bar, close it
-bg.onclick = function(event) {
-    TurnsNBOn.style.display = "block";
-	addani.classList.remove("rnavanimation");
-	hambaroff.style.display = "inline";
-	hambaron.style.display = "none";
-	bg.style.display = "none";
+function closebar() {
+	var mobile_r_nav = document.getElementById("responsive_mobile_navbar");
+	mobile_r_nav.classList.remove("mobile_responsive_navbar_open");
+	mobile_r_nav.classList.add("mobile_responsive_navbar_close");
+	var outsideofnavbg = document.getElementsByClassName("TheResponsiveNavBarbackground")[0];
+	outsideofnavbg.style.display = "none";
+	var onplus = document.getElementsByClassName("on")[0];
+	onplus.style.display = "none";
+	var offplus = document.getElementsByClassName("off")[0];
+	offplus.style.display = "inline";
 }
